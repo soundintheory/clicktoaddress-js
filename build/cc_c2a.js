fd416cd2baed06f07afbd7120ec2f0f798566ff1
Gabor Suranyi <gabor@craftyclicks.co.uk>
2018-05-15 08:52:13 +0100

    Tools / defaultDiacritics had incorrect scope

--- build/cc_c2a.js
+++ build/cc_c2a.js
@@ -1894,7 +1894,7 @@ clickToAddress.prototype.tools.defaultDiacriticsRemovalMap = [
 	{'base':'z','letters':/[\u007A\u24E9\uFF5A\u017A\u1E91\u017C\u017E\u1E93\u1E95\u01B6\u0225\u0240\u2C6C\uA763]/g}
 ];
 clickToAddress.prototype.tools.removeDiacritics = function(str) {
-	var changes = this.tools.defaultDiacriticsRemovalMap;
+	var changes = this.defaultDiacriticsRemovalMap;
 	for(var i=0; i<changes.length; i++) {
 		str = str.replace(changes[i].letters, changes[i].base);
 	}
